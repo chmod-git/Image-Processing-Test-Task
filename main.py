73f0fa2bb25a32cbb3e5a58e0872d4e14a7733ee
ValetovVitalii <valetov.vitalii@gmail.com>
2025-10-07 02:57:00 +0600

    file uploading and changing functions for simple brightness regulating application

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-import cv2
-import matplotlib.pyplot as plt
+import os
+from PIL import Image
+file_path = input("Enter the path to file: ")
 
-in_image = cv2.imread("girlc_small.bmp")
-plt.imshow(in_image)
-plt.show()
+if not os.path.isfile(file_path):
+    print("File not found.")
+    exit(1)
+try:
+    percent = float(input("Enter percent to increase brightness of image: "))
+except ValueError:
+    print("Error occured!")
+    exit(1)
+
+
+try:
+    image = Image.open(file_path)
+    print("Image successfully loaded.")
+except Exception as e:
+    print(f"Error loading image: {e}")
+    exit(1)
+
+
+output_path = input("Enter path to save the modified image: ")
+
+
+try:
+    modified_image.save(output_path)
+    print(f"Modified image saved to {output_path}.")
+except Exception as e:
+    print(f"Error saving image: {e}")
